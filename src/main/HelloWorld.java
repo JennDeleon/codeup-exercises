a099e43413ef646eb468ede566c2da0468297183
Jennifer Deleon <jennifer.deleon404@gmail.com>
2022-02-28 16:54:08 -0600

    done

---
+++ src/main/HelloWorld.java
@@ -0,0 +1,51 @@
+package main;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+//        System.out.println("Hello,");
+//        System.out.println(" World!");
+//        System.out.println("\n");
+//    }
+//    int myFavoriteNumber = 4;
+//        {
+//            System.out.println(myFavoriteNumber);
+//        }
+//        String myString = "3.1459";
+//        System.out.println(myString);
+
+
+//        long myNum = 1234L;
+//        System.out.println(myNum);
+
+//        float myNum = 3.14f;
+//        System.out.println(myNum);
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+//        System.out.println("three");
+
+//        int x = 4;
+//        x = x + 5;
+
+//        int myNum 4;
+//        int MyNum += 5;
+//
+
+//        int x = 3;
+//        int y = 4;
+//        y = y * x;
+
+
+//
+//        int x = 10;
+//        int y = 2;
+//        x = x / y;
+//        y = y - x;
+
+    }
+}
