b320dbb20827a9bef0a58b8d8090190154c04619
Jennifer Deleon <jennifer.deleon404@gmail.com>
2022-03-03 10:52:51 -0600

    finished

---
+++ src/main/Bob.java
@@ -0,0 +1,41 @@
+package main;
+//    Create a class named Bob with a main method for the following exercise.
+//
+//    Bob is a lackadaisical teenager. In conversation, his responses are very limited.
+//
+//    Bob answers 'Sure.' if you ask him a question. (the input ends with a question mark)
+//    He answers 'Whoa, chill out!' if you yell at him. (the input ends with an exclamation mark)
+//    He says 'Fine. Be that way!' if you address him without actually saying anything. (empty input)
+//    He answers 'Whatever.' to anything else.
+
+import java.util.Scanner;
+
+public class Bob {
+    public static void main (String[] args) {
+        Scanner input = new Scanner(System.in);
+//        System.out.println(userInput);
+
+        System.out.print("Talk to moody Bob:");
+
+        String Sure = "Sure.";
+        String ChillOut = "Whoa, chill out!";
+        String Fine = "Fine. Be that way!";
+        String Whatever = "Whatever.";
+
+        do {
+            String userInput = input.nextLine().trim();
+//            System.out.println(userInput);
+            if (userInput.contains("!")) {
+                System.out.println(ChillOut);
+            } else if (userInput.equals("")) {
+                System.out.println(Fine);
+            } else if (userInput.contains("?")) {
+                System.out.println(Sure);
+            } else {
+                System.out.println(Whatever);
+                System.out.print("Talk to moody Bob:");
+            }
+        } while (input.hasNext());
+    }
+
+    }
--- src/main/StringExercise.java
+++ src/main/StringExercise.java
@@ -2,19 +2,19 @@ package main;
 
 public class StringExercise {
     public static void main (String[] args){
-        String PinkFloyd = ("We don't need no education\n" +
+        String pinkFloyd = ("We don't need no education\n" +
                 "We don't need no thought control");
-        System.out.println(PinkFloyd);
+        System.out.println(pinkFloyd);
 
         String s = ("Check \"this\" out!, \"s inside of \"s!");
         System.out.println(s);
 
-        String C = "In windows, the main drive is usually C:\\";
-        System.out.println(C);
+        String c = "In windows, the main drive is usually C:\\";
+        System.out.println(c);
 
-        String BackSlash = ("I can do backslashes \\, double backslashes \\\\,\n" +
+        String backSlash = ("I can do backslashes \\, double backslashes \\\\,\n" +
                 "and the amazing triple backslash \\\\\\!");
-        System.out.println(BackSlash);
+        System.out.println(backSlash);
 
     }
 }
