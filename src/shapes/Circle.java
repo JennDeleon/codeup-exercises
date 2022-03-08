f5797b50c9bb5f1ba7b80829e3928447974572f7
Jennifer Deleon <jennifer.deleon404@gmail.com>
2022-03-08 09:44:23 -0600

    done

--- src/shapes/Circle.java
+++ src/shapes/Circle.java
@@ -1,2 +1,35 @@
-package shapes;public class Circle {
+package shapes;
+
+public class Circle {
+    private static int numCircles = 0;
+
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+
+    public double getCircumference() {
+        return 2 * Math.PI * radius;
+    }
+
+    public static int getNumCircles() {
+        return numCircles;
+    }
+
+    public static void setNumCircles(int numCircles) {
+        Circle.numCircles = numCircles;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
 }
--- src/shapes/CircleApp.java
+++ src/shapes/CircleApp.java
@@ -1,2 +1,31 @@
-package shapes;public class CircleApp {
+package shapes;
+
+import util.Input;
+
+public class CircleApp {
+
+    public static void main(String[] args) {
+        Input input = new Input();
+
+        // loop until user says no more circles
+        while (true) {
+            doACircle(input);
+
+            boolean moreCircles = input.yesNo("More circles? y/n ");
+            if(!moreCircles) {
+                break;
+            }
+        }
+
+        System.out.println("# circles created: " + Circle.getNumCircles());
+    }
+
+    private static void doACircle(Input input) {
+//        double radius = input.getDouble("Enter circle radius: ");
+//        Circle circle = new Circle(radius);
+        Circle.setNumCircles(Circle.getNumCircles() + 1);
+
+//        System.out.printf("Area of circle with radius %f is %f\n", radius, circle.getArea());
+//        System.out.printf("Circumference of circle with radius %f is %f\n", radius, circle.getCircumference());
+    }
 }
