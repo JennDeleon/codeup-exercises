66fef593ec78764eada89f5c3d32170369e70da9
Jennifer Deleon <jennifer.deleon404@gmail.com>
2022-03-10 09:12:10 -0600

    reviewing and playing with code

--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -1,7 +1,9 @@
 package shapes;
 
 public class ShapesTest {
+
     public static void main(String[] args) {
+
         Rectangle box1 = new Rectangle(5, 4);
         Rectangle box2 = new Square(5);
         ((Square) box2).setSize(6);
