f95342ccb689b5fe558702a5e273f070f6f02f7f
Jennifer Deleon <jennifer.deleon404@gmail.com>
2022-03-13 16:18:27 -0500

    fixed an arror on server name generator

--- src/main/ServerNameGenerator.java
+++ src/main/ServerNameGenerator.java
@@ -2,13 +2,14 @@ package main;
 
 public class ServerNameGenerator {
     public static void main (String[] args) {
-        String [] adjectives = {"tall", "short", "wet", "dry", "clean", "hot", "cold"};
-        String [] nouns = {"dog", "cat", "fish", "racoon", "computer", "Texas"};
+        String [] adjectives = {"tall", "short", "wet", "dry", "clean", "dirty", "hot", "cold"};
+        String [] nouns = {"dog", "cat", "fish", "racoon", "computer", "Texas", "Starbucks", "coffee"};
 
         String adjective = random(adjectives);
         String noun = random(nouns);
 
-        System.out.printf("Here is your servers name:" + adjective + noun);
+        System.out.printf("Your servers name is:\n%s-%s", adjective, noun);
+
     }
     private static String random(String[] stringArray) {
         int index = (int) (Math.random() * stringArray.length);
